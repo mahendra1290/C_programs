155cdc4dc1f4d8bc5edf81c12dad23c054a5447f
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2018-11-18 17:39:41 +0530

    work on progress

--- factill35.c
+++ factill35.c
@@ -4,73 +4,32 @@
 #include<stdio.h>
 
 int main(){
-    int ar_1[200];  //unit array
-	int ar_2[200];  //tenth array
+	int ar_1[200];
+	int ar_2[200];
 	int sum[200];
-	int j = 200;
+	int a, b;
+	for(int p=0; p<200; p++){
+		sum[p] = 1;
+	}
 	int unit;
-	while(j--){
-		ar_1[j] = 0;
-		ar_2[j] = 0;
-		sum[j] = 1;
-	}
+	int cary;
 	int temp;
-	int cary=0;
 	int num;
-	scanf("%d", &num);
-	int i=1;
-	unsigned long int count = 0;
+	scanf("%d",&num);
+	int i = 1;
 	while(num>0){
-		count++;
-		int k;
+		int j=0;
+		a = num%10;
+		b = num/10;
 		for(int t=0; t<i; t++){
-		temp = sum[t]*num;
-		unit = temp%10;
-		cary = temp/10;
-		ar_1[t] = unit;
-		ar_2[t] = cary;
-	//	printf("un %d cr %d\n",unit,cary);
-	    }
-	    if(i == 1){
-	    	sum[0] = ar_1[0];
-	    	sum[1] = ar_2[0];
-	    	i++;
-		    num -= 1;
-		    continue;
-		}
-		cary = 0;
-		for(k=0; k<i ; k++){
-			count++;
-		//	printf("yess\n");
-			if(!k){
-				sum[0] = ar_1[0];
-				//printf("s = %d\n",sum[0]);
+			temp = sum[t]*a;
+			while(temp>0){
+				int u = temp%10;
+				int c = temp/10;
+				
 			}
-			else{
-				sum[k] = ar_1[k] + ar_2[k-1] + cary;
-				cary = sum[k]/10;
-				sum[k] = sum[k]%10;
-			}
-			if(k == i-1){
-				sum[i] = ar_2[k] + cary;
-				cary = 0;
-			}
-		}
-		if(!sum[i]){
-			sum[i] = 1;
-			i--;
-		}
-		//printf("sum %d = %d\n", i, sum[i]);
-		i++;
-		num -= 1;
-	    //printf("\n");
-		if(num == 1){
-			break;
 		}
 	}
-	 for(int l=1; l<=i; l++){
-		    printf("%d", sum[i-l]);
-	    }
-	    printf("\n**%d", count);
+	
 }
   
