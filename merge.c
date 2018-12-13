e09bb9343792c784d1abf0c20457d5deb67b5018
mahendra1290 <42772952+mahendra1290@users.noreply.github.com>
2018-12-13 21:59:04 +0530

    Update merge.c

--- merge.c
+++ merge.c
@@ -1,30 +1,35 @@
 #include<stdio.h>
 //function for splitting
-void * mergesort(int [] , int , int );
+void mergesort(int [] , int );
 void merge(int [], int [] , int [] , int , int );
 
-void * mergesort(int arr[], int start, int end){
+void mergesort(int arr[], int end){
+	int start = 0;
 	if(end-start <= 1){                      //checks if only one item is left in list
-		return arr;              
+		return ;
 	}
 	else{
 	int mid = (end-start)/2;                 //split list from start-mid and mid-end
 	int left[mid];                           //left part
 	int right[end-mid];                      //right part
-	    for(int i=0; i<mid; i++){            //creating left part 
+	    for(int i=0; i<mid; i++){            //creating left part
 		left[i] = arr[i];
+		printf("%d ", left[i]);
 	    }
+	    printf("\n");
 	for(int i=0; i<end-mid; i++){            //creating right part
 		right[i] = arr[i+mid];
-	    } 
-	mergesort(left, start, mid);             //reccursively calling
-	mergesort(right, start, end-mid);
+		printf("%d ", right[i]);
+	    }
+		printf("\n");
+	mergesort(left, mid);             //reccursively calling
+	mergesort(right, end-mid);
 	merge(arr, left, right, sizeof(left)/4, sizeof(right)/4);
     }
 }
 void merge(int a[], int l[], int r[], int len_l, int len_r){
 	int k = len_l + len_r;                   //length of our merged array
-	int i=0,j=0,x=0;                         //i and j are index for left and right respectively
+	int i=0,j=0,x=0,v;                       //i and j are index for left and right respectively
 	while(x < k && (i<len_l && j<len_r)){    //iterating until we have completed our merged arrays
 		if(l[i] < r[j]){                     //checks if left side item is smaller than right
 			a[x] = l[i];
@@ -48,6 +53,7 @@ void merge(int a[], int l[], int r[], int len_l, int len_r){
 	        }
 	    }
 	}
-}	
+}
+
 
 
